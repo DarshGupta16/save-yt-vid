1606825af4c04cb233d19a0da3804ac9c2a63e5a
DarshGupta16 <darsh.gupta.1602@gmail.com>
2026-05-20 15:06:55 +0530

    Get the basic app shell up and running

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { readFileSync, writeFileSync } from "fs";
+import { sleep } from "bun";
 
-const app = new Hono()
+const execAsync = promisify(exec);
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const data = readFileSync("queue.json", "utf8");
+let queue = JSON.parse(data).queue as string[];
+let processing = false;
 
-export default app
+app.get("/save-video", (c) => {
+  try {
+    const url = c.req.query("url");
+
+    if (url) queue.push(url);
+    else throw "url param not provided!";
+
+    writeFileSync("queue.json", JSON.stringify({ queue }));
+    processNext();
+    return c.json({ queued: true });
+  } catch (error) {
+    return c.json({ queued: false, error });
+  }
+});
+
+const processNext = async () => {
+  if (processing) return;
+  if (queue.length === 0) return;
+
+  processing = true;
+  // All processing work here
+  console.log(`processing ${queue[0]}`);
+  await sleep(10000);
+  // Finished processing
+  processing = false;
+  queue.shift();
+  writeFileSync("queue.json", JSON.stringify({ queue }));
+
+  processNext();
+};
+
+export default app;
